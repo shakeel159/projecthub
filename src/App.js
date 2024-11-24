8394e3e95d78dc8fe360d824a3f6ac20a2cd0989
shakeel159 <jazibshaq@gmail.com>
2024-11-24 13:17:05 -0600

    Initial commit
    forgot to commit and already added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import Hawkimg from "./Images/hawk_sky.jpg"
 import './App.css';
 
 function App() {
+  const [imageSrc, setImageSrc] = useState(Hawkimg);
+
+  const updateImage = () => {
+    setImageSrc(Hawkimg);
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
+      <body>
+          <div className="Background-Image-Box">
+            <div className="Background-Image-Content"> 
+              <img src={imageSrc} alt="Dynamic"></img>
+            </div>
+          </div>
+      </body>
+      <footer>
+        <div className="Footer-Background">
+          <div className="Footer-Conent">
+            <h1>Footer</h1>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
